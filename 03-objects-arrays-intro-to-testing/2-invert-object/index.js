79f8056864751cbe38336645f83fe632155566e5
maks <example@gmail.com>
2024-09-26 00:59:12 +0300

    Решение 03-objects-arrays-intro-to-testing/2-invert-object

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,16 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (obj === undefined || Object.keys(obj).length === 0) {
+    return obj;
+  }
+      
+  const pairs = Object.entries(obj);
+  const newObj = {};
+      
+  for (const [key, val] of pairs) {
+    newObj[val] = key;
+  }
+      
+  return newObj;
 }
