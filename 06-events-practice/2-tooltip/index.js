bac30180e47dbe5629b967422cef86b318687e2e
maks <example@gmail.com>
2024-10-09 19:50:19 +0300

    Решение 06-events-practice/2-tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,81 @@
 class Tooltip {
-  initialize () {
+  static _instance;
 
+  constructor() {
+    if (Tooltip._instance) {
+      return Tooltip._instance;
+    }
+
+    Tooltip._instance = this;
+    this.initialize();
+  }
+
+  initialize () {
+    this.element = this._createElement();
+    this._createDocumentListeners();
+  }
+
+  _createDocumentListeners() {
+    document.addEventListener('pointerover', this._handlePointerOver);
+  }
+
+  _destroyDocumentListeners() {
+    document.removeEventListener('pointerover', this._handlePointerOver);
+  }
+
+  _createTargetListeners() {
+    this.target.addEventListener('pointerout', this._handlePointerOut);
+  }
+
+  _destroyTargetListeners() {
+    this.target.removeEventListener('pointerout', this._handlePointerOut);
+  }
+
+  _handlePointerOver = (e) => {
+    const tooltip = e.target.dataset.tooltip;
+
+    if (!tooltip) {
+      return;
+    }
+  
+    this.target = e.target;
+    this._createTargetListeners();
+    
+    this.render(tooltip, e.clientX, e.clientY);
+  }
+
+  _handlePointerOut = () => {
+    this._destroyTargetListeners(this.target);
+    this.remove();
+  }
+
+  _createElement() {
+    const tooltipElement = document.createElement('div');
+    tooltipElement.innerHTML = `<div class="tooltip"></div>`;
+
+    return tooltipElement.firstElementChild;
+  }
+
+  render(tooltip, x, y) {
+    this.element.textContent = tooltip;
+
+    this.element.style.left = x + "px";
+    this.element.style.top = y + "px";
+
+    document.body.append(this.element);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this._destroyDocumentListeners();
+    if (this.target) {
+      this._destroyTargetListeners();
+    }
+
+    this.remove();
   }
 }
 
